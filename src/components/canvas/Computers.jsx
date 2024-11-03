3cb8d61762bf49efd5960368561bc16b99961bdb
skaduhs5232 <thiagooliveira1039@gmail.com>
2024-11-03 20:44:42 -0300

    tirando o computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,17 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  if (isMobile) {
-    return (
-      <img
-        src="https://via.placeholder.com/300" // Coloque o link para a imagem desejada
-        alt="Computador"
-        style={{ width: "100%", height: "auto" }}
-      />
-    );
-  }
-
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+ const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
@@ -31,8 +21,8 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -43,22 +33,27 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
+    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
 
+    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
+    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
   }, []);
 
-  return isMobile ? (
-    <Computers isMobile={isMobile} />
-  ) : (
+  return (
     <Canvas
       frameloop='demand'
       shadows
